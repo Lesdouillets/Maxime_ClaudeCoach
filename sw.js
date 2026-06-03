0f5dafb09ec9127947baf5b244a0e3ea46042c41
Lesdouillets <lesdouillets@gmail.com>
2026-06-03 11:19:27 +0000

    Deploying to gh-pages from @ Lesdouillets/Maxime_ClaudeCoach@2ffd798e2478d42c7bdff575dfef45511fe4bf6a 🚀

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 // Strategy: network-first for HTML navigation, cache-first for hashed static assets.
 // skipWaiting + clients.claim ensure updates apply immediately on next open.
 
-const STATIC_CACHE = "cc-static-1813425";
+const STATIC_CACHE = "cc-static-2ffd798";
 
 self.addEventListener("install", () => {
   self.skipWaiting(); // activate immediately, don't wait for old SW to finish
